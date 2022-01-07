2383f66a7a9c93c4add97b6ae6abed0970fbb280
0709nvanh <86367357+0709nvanh@users.noreply.github.com>
2022-01-07 15:52:38 +0700

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,10 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
+        semi: ["error", "always"],
         quotes: ["error", "double", { allowTemplateLiterals: true }],
         indent: ["error", 4],
+        "linebreak-style": 0,
         "eol-last": ["error", "never"],
     }
 };
